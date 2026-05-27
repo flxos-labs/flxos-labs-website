4e73510204bf2dc9c1b4f4ddf891402a2089bc61
Akash <145587932+Itsmeakash248@users.noreply.github.com>
2026-05-27 21:37:14 +0530

    feat: configure image remote patterns and enhance Supabase client error handling

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "github.com",
+        pathname: "/Itsmeakash248.png",
+        search: "?size=200",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
